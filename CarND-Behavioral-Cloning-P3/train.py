f7863e31d75835d4eb69369238589c09fc2ac90e
srogawsk <sebastian.rogawski@intel.com>
2017-07-12 01:51:14 +0200

    further data augmentation, taking advantage of multi cameras

--- CarND-Behavioral-Cloning-P3/train.py
+++ CarND-Behavioral-Cloning-P3/train.py
@@ -2,30 +2,53 @@ import csv
 import cv2
 import numpy as np
 
-lines = []
+car_images = []
+steering_angles = []
 
-with open('../../../data_track_1_320x240/driving_log.csv') as csvfile:
+def car_images_extend(img_center, img_left, imge_right):
+    car_images.append(img_center)
+    car_images.append(img_left)
+    car_images.append(img_right)
+    car_images.append(np.fliplr(img_center))
+    car_images.append(np.fliplr(img_left))
+    car_images.append(np.fliplr(img_right))
+
+
+def steering_angles_extend(steering_center, steering_left, steering_right):
+    steering_angles.append(steering_center)
+    steering_angles.append(steering_left)
+    steering_angles.append(steering_right)
+    steering_angles.append(-steering_center)
+    steering_angles.append(-steering_left)
+    steering_angles.append(-steering_right)
+
+
+with open("../../../data_track_1_320x240/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+    for row in reader:
+        steering_center = float(row[3])
 
-images = [] 
-measurements = []
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('\\')[-1]
-    current_path = '../../../data_track_1_320x240/IMG/' + filename
-    image = cv2.imread(current_path)
-    image_flipped = np.fliplr(image)
-    images.append(image)
-    images.append(image_flipped)
-    measurement = float(line[3])
-    measurement_flipped = -measurement
-    measurements.append(measurement)
-    measurements.append(measurement_flipped)
+        # create adjusted steering measurements for the side camera images
+        correction = 0.2 # this is a parameter to tune
+        steering_left = steering_center + correction
+        steering_right = steering_center - correction
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+        # read in images from center, left and right cameras
+        path = "../../../data_track_1_320x240/IMG/" # fill in the path to your training IMG directory
+        
+        img_center = cv2.imread(path + row[0].split('\\')[-1])
+        img_left = cv2.imread(path + row[1].split('\\')[-1])
+        img_right = cv2.imread(path + row[2].split('\\')[-1])
+	
+        # add images and angles to data set
+        #car_images.extend(img_center, img_left, img_right)
+        car_images_extend(img_center, img_left, img_right)
+        #steering_angles.extend(steering_center, steering_left, steering_right)
+        steering_angles_extend(steering_center, steering_left, steering_right)
+
+
+X_train = np.array(car_images)
+y_train = np.array(steering_angles)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
