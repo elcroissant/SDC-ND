f299b59dc9dd13a8282daad6c85a9edca3964d55
srogawsk <sebastian.rogawski@intel.com>
2017-07-12 02:07:29 +0200

    Image cropping added

--- CarND-Behavioral-Cloning-P3/train.py
+++ CarND-Behavioral-Cloning-P3/train.py
@@ -54,8 +54,11 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.layers import Cropping2D
+
 model = Sequential()
 model.add(Lambda( lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(6, 5, 5, border_mode='valid', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2), border_mode='valid'))
 model.add(Convolution2D(16, 5, 5, border_mode='valid', activation='relu'))
