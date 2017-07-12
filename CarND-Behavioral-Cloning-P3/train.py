83e260a3df9198a5a9979b31d007f3beb42fd5b8
srogawsk <sebastian.rogawski@intel.com>
2017-07-13 01:28:10 +0200

    3 laps of center driving + recovery driving + clock-wise driving + dropout 0.5

--- CarND-Behavioral-Cloning-P3/train.py
+++ CarND-Behavioral-Cloning-P3/train.py
@@ -2,56 +2,64 @@ import csv
 import cv2
 import numpy as np
 
-car_images = []
-steering_angles = []
 
-def car_images_extend(img_center, img_left, imge_right):
-    car_images.append(img_center)
-    car_images.append(img_left)
-    car_images.append(img_right)
-    car_images.append(np.fliplr(img_center))
-    car_images.append(np.fliplr(img_left))
-    car_images.append(np.fliplr(img_right))
+class CarInfo:
+    def __init__(self):
+        self.car_images = []
+        self.steering_angles = []
+
+    def car_images_extend(self, img_center, img_left, img_right):
+        self.car_images.append(img_center)
+        self.car_images.append(img_left)
+        self.car_images.append(img_right)
+        self.car_images.append(np.fliplr(img_center))
+        self.car_images.append(np.fliplr(img_left))
+        self.car_images.append(np.fliplr(img_right))
+    
+    def steering_angles_extend(self, steering_center, steering_left, steering_right):
+        self.steering_angles.append(steering_center)
+        self.steering_angles.append(steering_left)
+        self.steering_angles.append(steering_right)
+        self.steering_angles.append(-steering_center)
+        self.steering_angles.append(-steering_left)
+        self.steering_angles.append(-steering_right)
+    
+
+def collect_data(csv_path, img_path):
+    with open(csv_path) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            steering_center = float(row[3])
+    
+            # create adjusted steering measurements for the side camera images
+            correction = 0.2 # this is a parameter to tune
+            steering_left = steering_center + correction
+            steering_right = steering_center - correction
+    
+            # read in images from center, left and right cameras
+            path = img_path # fill in the path to your training IMG directory
+            
+            img_center = cv2.imread(path + row[0].split('\\')[-1])
+            img_left = cv2.imread(path + row[1].split('\\')[-1])
+            img_right = cv2.imread(path + row[2].split('\\')[-1])
+    
+            # add images and angles to data set
+            car_info.car_images_extend(img_center, img_left, img_right)
+            car_info.steering_angles_extend(steering_center, steering_left, steering_right)
+
+car_info = CarInfo()	
+
+collect_data("../../data_track_1_adv/driving_log.csv", "../../data_track_1_adv/IMG/")
+collect_data("../../data_track_1_adv_revert/driving_log.csv", "../../data_track_1_adv_revert/IMG/")
+collect_data("../../data_track_1_adv_recovery/driving_log.csv", "../../data_track_1_adv_recovery/IMG/")
 
 
-def steering_angles_extend(steering_center, steering_left, steering_right):
-    steering_angles.append(steering_center)
-    steering_angles.append(steering_left)
-    steering_angles.append(steering_right)
-    steering_angles.append(-steering_center)
-    steering_angles.append(-steering_left)
-    steering_angles.append(-steering_right)
 
-
-with open("../../../data_track_1/driving_log.csv") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        steering_center = float(row[3])
-
-        # create adjusted steering measurements for the side camera images
-        correction = 0.2 # this is a parameter to tune
-        steering_left = steering_center + correction
-        steering_right = steering_center - correction
-
-        # read in images from center, left and right cameras
-        path = "../../../data_track_1/IMG/" # fill in the path to your training IMG directory
-        
-        img_center = cv2.imread(path + row[0].split('\\')[-1])
-        img_left = cv2.imread(path + row[1].split('\\')[-1])
-        img_right = cv2.imread(path + row[2].split('\\')[-1])
-	
-        # add images and angles to data set
-        #car_images.extend(img_center, img_left, img_right)
-        car_images_extend(img_center, img_left, img_right)
-        #steering_angles.extend(steering_center, steering_left, steering_right)
-        steering_angles_extend(steering_center, steering_left, steering_right)
-
-
-X_train = np.array(car_images)
-y_train = np.array(steering_angles)
+X_train = np.array(car_info.car_images)
+y_train = np.array(car_info.steering_angles)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
@@ -66,8 +74,11 @@ model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
+model.add(Dropout(0.5))
 model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
